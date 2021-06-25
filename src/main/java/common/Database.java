4bd6a78374f61e31518378fba0811cdbcd45813f
memoeslink <memocad@gmail.com>
2021-06-25 17:50:44 -0500

    Added methods to get random table rows in Database.java.

--- src/main/java/common/Database.java
+++ src/main/java/common/Database.java
@@ -107,48 +107,76 @@ public class Database {
         return countRows(TABLE_ENGLISH_ADJECTIVES);
     }
 
+    public static String selectEnglishAdjective() {
+        return selectRow("SELECT * FROM " + TABLE_ENGLISH_ADJECTIVES + " ORDER BY RANDOM() LIMIT 1", 2);
+    }
+
     public static String selectEnglishAdjective(int id) {
-        return selectRow("SELECT * FROM " + TABLE_ENGLISH_ADJECTIVES + " WHERE " + TABLE_ENGLISH_ADJECTIVES + ID_PREFIX + " = ?", 2, id);
+        return selectRow("SELECT * FROM " + TABLE_ENGLISH_ADJECTIVES + " WHERE " + TABLE_ENGLISH_ADJECTIVES + ID_PREFIX + " = ?", 2, String.valueOf(id));
     }
 
     public static int countEnglishFemaleNames() {
         return countRows(TABLE_ENGLISH_FEMALE_NAMES);
     }
 
+    public static String selectEnglishFemaleName() {
+        return selectRow("SELECT * FROM " + TABLE_ENGLISH_FEMALE_NAMES + " ORDER BY RANDOM() LIMIT 1", 2);
+    }
+
     public static String selectEnglishFemaleName(int id) {
-        return selectRow("SELECT * FROM " + TABLE_ENGLISH_FEMALE_NAMES + " WHERE " + TABLE_ENGLISH_FEMALE_NAMES + ID_PREFIX + " = ?", 2, id);
+        return selectRow("SELECT * FROM " + TABLE_ENGLISH_FEMALE_NAMES + " WHERE " + TABLE_ENGLISH_FEMALE_NAMES + ID_PREFIX + " = ?", 2, String.valueOf(id));
     }
 
     public static int countEnglishMaleNames() {
         return countRows(TABLE_ENGLISH_MALE_NAMES);
     }
 
+    public static String selectEnglishMaleName() {
+        return selectRow("SELECT * FROM " + TABLE_ENGLISH_MALE_NAMES + " ORDER BY RANDOM() LIMIT 1", 2);
+    }
+
     public static String selectEnglishMaleName(int id) {
-        return selectRow("SELECT * FROM " + TABLE_ENGLISH_MALE_NAMES + " WHERE " + TABLE_ENGLISH_MALE_NAMES + ID_PREFIX + " = ?", 2, id);
+        return selectRow("SELECT * FROM " + TABLE_ENGLISH_MALE_NAMES + " WHERE " + TABLE_ENGLISH_MALE_NAMES + ID_PREFIX + " = ?", 2, String.valueOf(id));
     }
 
     public static int countEnglishNouns() {
         return countRows(TABLE_ENGLISH_NOUNS);
     }
 
+    public static String selectEnglishNoun() {
+        return selectRow("SELECT * FROM " + TABLE_ENGLISH_NOUNS + " ORDER BY RANDOM() LIMIT 1", 2);
+    }
+
     public static String selectEnglishNoun(int id) {
-        return selectRow("SELECT * FROM " + TABLE_ENGLISH_NOUNS + " WHERE " + TABLE_ENGLISH_NOUNS + ID_PREFIX + " = ?", 2, id);
+        return selectRow("SELECT * FROM " + TABLE_ENGLISH_NOUNS + " WHERE " + TABLE_ENGLISH_NOUNS + ID_PREFIX + " = ?", 2, String.valueOf(id));
     }
 
     public static int countEnglishOccupations() {
         return countRows(TABLE_ENGLISH_OCCUPATIONS);
     }
 
+    public static String selectEnglishOccupation() {
+        return selectRow("SELECT * FROM " + TABLE_ENGLISH_OCCUPATIONS + " ORDER BY RANDOM() LIMIT 1", 2);
+    }
+
     public static String selectEnglishOccupation(int id) {
-        return selectRow("SELECT * FROM " + TABLE_ENGLISH_OCCUPATIONS + " WHERE " + TABLE_ENGLISH_OCCUPATIONS + ID_PREFIX + " = ?", 2, id);
+        return selectRow("SELECT * FROM " + TABLE_ENGLISH_OCCUPATIONS + " WHERE " + TABLE_ENGLISH_OCCUPATIONS + ID_PREFIX + " = ?", 2, String.valueOf(id));
     }
 
     public static int countEnglishPhonetics() {
         return countRows(TABLE_ENGLISH_PHONETICS);
     }
 
+    public static String selectEnglishWord() {
+        return selectRow("SELECT * FROM " + TABLE_ENGLISH_PHONETICS + " ORDER BY RANDOM() LIMIT 1", 2);
+    }
+
     public static String selectEnglishWord(int id) {
-        return selectRow("SELECT * FROM " + TABLE_ENGLISH_PHONETICS + " WHERE " + TABLE_ENGLISH_PHONETICS + ID_PREFIX + " = ?", 2, id);
+        return selectRow("SELECT * FROM " + TABLE_ENGLISH_PHONETICS + " WHERE " + TABLE_ENGLISH_PHONETICS + ID_PREFIX + " = ?", 2, String.valueOf(id));
+    }
+
+    public static String selectEnglishPhoneticScript() {
+        return selectRow("SELECT Word, Phonetics FROM " + TABLE_ENGLISH_PHONETICS + " ORDER BY RANDOM() LIMIT 1", 2);
     }
 
     public static String selectEnglishPhoneticScript(String word) {
@@ -159,103 +187,155 @@ public class Database {
         return countRows(TABLE_ENGLISH_SURNAMES);
     }
 
+    public static String selectEnglishSurname() {
+        return selectRow("SELECT * FROM " + TABLE_ENGLISH_SURNAMES + " ORDER BY RANDOM() LIMIT 1", 2);
+    }
+
     public static String selectEnglishSurname(int id) {
-        return selectRow("SELECT * FROM " + TABLE_ENGLISH_SURNAMES + " WHERE " + TABLE_ENGLISH_SURNAMES + ID_PREFIX + " = ?", 2, id);
+        return selectRow("SELECT * FROM " + TABLE_ENGLISH_SURNAMES + " WHERE " + TABLE_ENGLISH_SURNAMES + ID_PREFIX + " = ?", 2, String.valueOf(id));
     }
 
     public static int countHispanicCompoundSurnames() {
         return countRows(TABLE_SPANISH_COMPOUND_SURNAMES);
     }
 
+    public static String selectHispanicCompoundSurname() {
+        return selectRow("SELECT * FROM " + TABLE_SPANISH_COMPOUND_SURNAMES + " ORDER BY RANDOM() LIMIT 1", 2);
+    }
+
     public static String selectHispanicCompoundSurname(int id) {
-        return selectRow("SELECT * FROM " + TABLE_SPANISH_COMPOUND_SURNAMES + " WHERE " + TABLE_SPANISH_COMPOUND_SURNAMES + ID_PREFIX + " = ?", 2, id);
+        return selectRow("SELECT * FROM " + TABLE_SPANISH_COMPOUND_SURNAMES + " WHERE " + TABLE_SPANISH_COMPOUND_SURNAMES + ID_PREFIX + " = ?", 2, String.valueOf(id));
     }
 
     public static int countHispanicFemaleNames() {
         return countRows(TABLE_SPANISH_FEMALE_NAMES);
     }
 
+    public static String selectHispanicFemaleName() {
+        return selectRow("SELECT * FROM " + TABLE_SPANISH_FEMALE_NAMES + " ORDER BY RANDOM() LIMIT 1", 2);
+    }
+
     public static String selectHispanicFemaleName(int id) {
-        return selectRow("SELECT * FROM " + TABLE_SPANISH_FEMALE_NAMES + " WHERE " + TABLE_SPANISH_FEMALE_NAMES + ID_PREFIX + " = ?", 2, id);
+        return selectRow("SELECT * FROM " + TABLE_SPANISH_FEMALE_NAMES + " WHERE " + TABLE_SPANISH_FEMALE_NAMES + ID_PREFIX + " = ?", 2, String.valueOf(id));
     }
 
     public static int countHispanicMaleNames() {
         return countRows(TABLE_SPANISH_MALE_NAMES);
     }
 
+    public static String selectHispanicMaleName() {
+        return selectRow("SELECT * FROM " + TABLE_SPANISH_MALE_NAMES + " ORDER BY RANDOM() LIMIT 1", 2);
+    }
+
     public static String selectHispanicMaleName(int id) {
-        return selectRow("SELECT * FROM " + TABLE_SPANISH_MALE_NAMES + " WHERE " + TABLE_SPANISH_MALE_NAMES + ID_PREFIX + " = ?", 2, id);
+        return selectRow("SELECT * FROM " + TABLE_SPANISH_MALE_NAMES + " WHERE " + TABLE_SPANISH_MALE_NAMES + ID_PREFIX + " = ?", 2, String.valueOf(id));
     }
 
     public static int countSpanishNouns() {
         return countRows(TABLE_SPANISH_NOUNS);
     }
 
+    public static String selectSpanishNoun() {
+        return selectRow("SELECT * FROM " + TABLE_SPANISH_NOUNS + " ORDER BY RANDOM() LIMIT 1", 2);
+    }
+
     public static String selectSpanishNoun(int id) {
-        return selectRow("SELECT * FROM " + TABLE_SPANISH_NOUNS + " WHERE " + TABLE_SPANISH_NOUNS + ID_PREFIX + " = ?", 2, id);
+        return selectRow("SELECT * FROM " + TABLE_SPANISH_NOUNS + " WHERE " + TABLE_SPANISH_NOUNS + ID_PREFIX + " = ?", 2, String.valueOf(id));
     }
 
     public static int countSpanishOccupations() {
         return countRows(TABLE_SPANISH_OCCUPATIONS);
     }
 
+    public static String selectSpanishOccupation() {
+        return selectRow("SELECT * FROM " + TABLE_SPANISH_OCCUPATIONS + " ORDER BY RANDOM() LIMIT 1", 2);
+    }
+
     public static String selectSpanishOccupation(int id) {
-        return selectRow("SELECT * FROM " + TABLE_SPANISH_OCCUPATIONS + " WHERE " + TABLE_SPANISH_OCCUPATIONS + ID_PREFIX + " = ?", 2, id);
+        return selectRow("SELECT * FROM " + TABLE_SPANISH_OCCUPATIONS + " WHERE " + TABLE_SPANISH_OCCUPATIONS + ID_PREFIX + " = ?", 2, String.valueOf(id));
     }
 
     public static int countSpanishPluralAdjectives() {
         return countRows(TABLE_SPANISH_PLURAL_ADJECTIVES);
     }
 
+    public static String selectSpanishPluralAdjective() {
+        return selectRow("SELECT * FROM " + TABLE_SPANISH_PLURAL_ADJECTIVES + " ORDER BY RANDOM() LIMIT 1", 2);
+    }
+
     public static String selectSpanishPluralAdjective(int id) {
-        return selectRow("SELECT * FROM " + TABLE_SPANISH_PLURAL_ADJECTIVES + " WHERE " + TABLE_SPANISH_PLURAL_ADJECTIVES + ID_PREFIX + " = ?", 2, id);
+        return selectRow("SELECT * FROM " + TABLE_SPANISH_PLURAL_ADJECTIVES + " WHERE " + TABLE_SPANISH_PLURAL_ADJECTIVES + ID_PREFIX + " = ?", 2, String.valueOf(id));
     }
 
     public static int countSpanishSingularAdjectives() {
         return countRows(TABLE_SPANISH_SINGULAR_ADJECTIVES);
     }
 
+    public static String selectSpanishSingularAdjective() {
+        return selectRow("SELECT * FROM " + TABLE_SPANISH_SINGULAR_ADJECTIVES + " ORDER BY RANDOM() LIMIT 1", 2);
+    }
+
     public static String selectSpanishSingularAdjective(int id) {
-        return selectRow("SELECT * FROM " + TABLE_SPANISH_SINGULAR_ADJECTIVES + " WHERE " + TABLE_SPANISH_SINGULAR_ADJECTIVES + ID_PREFIX + " = ?", 2, id);
+        return selectRow("SELECT * FROM " + TABLE_SPANISH_SINGULAR_ADJECTIVES + " WHERE " + TABLE_SPANISH_SINGULAR_ADJECTIVES + ID_PREFIX + " = ?", 2, String.valueOf(id));
     }
 
     public static int countHispanicSurnames() {
         return countRows(TABLE_SPANISH_SURNAMES);
     }
 
+    public static String selectHispanicSurname() {
+        return selectRow("SELECT * FROM " + TABLE_SPANISH_SURNAMES + " ORDER BY RANDOM() LIMIT 1", 2);
+    }
+
     public static String selectHispanicSurname(int id) {
-        return selectRow("SELECT * FROM " + TABLE_SPANISH_SURNAMES + " WHERE " + TABLE_SPANISH_SURNAMES + ID_PREFIX + " = ?", 2, id);
+        return selectRow("SELECT * FROM " + TABLE_SPANISH_SURNAMES + " WHERE " + TABLE_SPANISH_SURNAMES + ID_PREFIX + " = ?", 2, String.valueOf(id));
     }
 
     public static int countFamilyNames() {
         return countRows(TABLE_FAMILY_NAMES);
     }
 
+    public static String selectFamilyName() {
+        return selectRow("SELECT * FROM " + TABLE_FAMILY_NAMES + " ORDER BY RANDOM() LIMIT 1", 2);
+    }
+
     public static String selectFamilyName(int id) {
-        return selectRow("SELECT * FROM " + TABLE_FAMILY_NAMES + " WHERE " + TABLE_FAMILY_NAMES + ID_PREFIX + " = ?", 2, id);
+        return selectRow("SELECT * FROM " + TABLE_FAMILY_NAMES + " WHERE " + TABLE_FAMILY_NAMES + ID_PREFIX + " = ?", 2, String.valueOf(id));
     }
 
     public static int countNames() {
         return countRows(TABLE_NAMES);
     }
 
+    public static String selectName() {
+        return selectRow("SELECT * FROM " + TABLE_NAMES + " ORDER BY RANDOM() LIMIT 1", 2);
+    }
+
     public static String selectName(int id) {
-        return selectRow("SELECT * FROM " + TABLE_NAMES + " WHERE " + TABLE_NAMES + ID_PREFIX + " = ?", 2, id);
+        return selectRow("SELECT * FROM " + TABLE_NAMES + " WHERE " + TABLE_NAMES + ID_PREFIX + " = ?", 2, String.valueOf(id));
     }
 
     public static int countCommonNouns() {
         return countRows(TABLE_NOUNS);
     }
 
+    public static String selectCommonNoun() {
+        return selectRow("SELECT * FROM " + TABLE_NOUNS + " ORDER BY RANDOM() LIMIT 1", 2);
+    }
+
     public static String selectCommonNoun(int id) {
-        return selectRow("SELECT * FROM " + TABLE_NOUNS + " WHERE " + TABLE_NOUNS + ID_PREFIX + " = ?", 2, id);
+        return selectRow("SELECT * FROM " + TABLE_NOUNS + " WHERE " + TABLE_NOUNS + ID_PREFIX + " = ?", 2, String.valueOf(id));
     }
 
     public static int countUsernames() {
         return countRows(TABLE_USERNAMES);
     }
 
+    public static String selectUsername() {
+        return selectRow("SELECT * FROM " + TABLE_USERNAMES + " ORDER BY RANDOM() LIMIT 1", 2);
+    }
+
     public static String selectUsername(int id) {
-        return selectRow("SELECT * FROM " + TABLE_USERNAMES + " WHERE " + TABLE_USERNAMES + ID_PREFIX + " = ?", 2, id);
+        return selectRow("SELECT * FROM " + TABLE_USERNAMES + " WHERE " + TABLE_USERNAMES + ID_PREFIX + " = ?", 2, String.valueOf(id));
     }
 }
