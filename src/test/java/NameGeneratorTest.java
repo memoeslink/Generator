d6382fa8ae50ad0a82060cb658c6f3ecd9a72872
memoeslink <memocad@gmail.com>
2021-10-09 16:10:27 -0500

    Added methods to NameGenerator.java.

--- src/test/java/NameGeneratorTest.java
+++ src/test/java/NameGeneratorTest.java
@@ -1,10 +1,25 @@
 import common.Constant;
 import common.NameGenerator;
+import common.NameType;
+import common.StringHelper;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class NameGeneratorTest {
+
+    @Test
+    public void getNameOrDefault() throws Exception {
+        NameGenerator nameGenerator = new NameGenerator();
+        assertNotEquals(StringHelper.EMPTY, nameGenerator.getNameOrDefault(NameType.EMPTY, "John Doe"));
+    }
+
+    @Test
+    public void getNameOrRetry() throws Exception {
+        NameGenerator nameGenerator = new NameGenerator();
+        assertNotEquals(StringHelper.EMPTY, nameGenerator.getNameOrRetry(NameType.DOUBLE_BARRELLED_MALE_FORENAME));
+    }
 
     @Test
     public void getDefaultName() throws Exception {
