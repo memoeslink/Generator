c44b8a27f611b88b6f9e5ac5f06c6b17dfd0e09d
memoeslink <memocad@gmail.com>
2022-04-06 17:49:41 -0500

    Refactored code.

--- src/main/java/common/StringGenerator.java
+++ src/main/java/common/StringGenerator.java
@@ -17,14 +17,13 @@ public class StringGenerator extends Generator {
     }
 
     public String getDecimalPercentage() {
-        int percentage = r.getInt(0, 101);
-        int decimals = r.getInt(0, 100);
-        return percentage == 100 ? "100.00%" : (percentage + "." + String.format(locale, "%02d", decimals) + "%");
+        float percentage = r.getFloat(0.0F, 100.0F);
+        return String.format(locale, "%.2f%%", percentage);
     }
 
     public String getStrColor(String s) {
         if (StringHelper.isNullOrEmpty(s))
-            return String.format("#%06X", (0xFFFFFF & -7829368));
+            return String.format("#%06x", (0xFFFFFF & -7829368));
         return String.format("#%06x", r.getInt(0xFFFFFF + 1));
     }
 }
