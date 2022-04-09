836e107bd5e04a489baee5ed5ed9451afab0ef6e
memoeslink <memocad@gmail.com>
2022-04-09 10:17:42 -0500

    Fixed some minor bugs.

--- src/main/java/common/Maker.java
+++ src/main/java/common/Maker.java
@@ -5,8 +5,10 @@ import com.memoeslink.common.Randomizer;
 public class Maker {
 
     public static String getColor(String s) {
+        if (StringHelper.isNullOrEmpty(s))
+            return String.format("#%06x", (0xFFFFFF & -7829368));
         StringGenerator stringGenerator = new StringGenerator(null, LongHelper.getSeed(s));
-        return stringGenerator.getStrColor(s);
+        return stringGenerator.getStrColor();
     }
 
     public static String getDefaultColor(String s) {
